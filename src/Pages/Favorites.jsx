a79b2845b48df452856ea85cd6c038c6e79e3faf
Manoj Rahar <manojrahar@192.168.1.2>
2026-03-04 20:08:33 +0530

    Fix favorites page grid spacing 2

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -9,7 +9,7 @@ const Favorites = () => {
     return (
       <div className="px-4 py-6">
         <h2 className="text-gray-300 font-bold text-4xl text-center m-3 p-3">Your Favorites</h2>
-        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 justify-items-center px-4">
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4 justify-center items-center px-4">
           {favorites.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
           ))}
